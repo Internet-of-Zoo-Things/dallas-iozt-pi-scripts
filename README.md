ea626ae58b05aefbe848e99efcd7ecb893b16a3b
Andrew Schwenn <aschwenn@verizon.net>
2021-03-22 22:45:19 -0500

    got first half of script working on pi

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This script should be ran once when setting up the Pi to download the required p
 # Boot script
 
 This script should run every time the Raspberry Pi boots, and will initialize all necessary servers/daemons. This can be ran from the `/home/pi/.bashrc` file as [described here](https://www.dexterindustries.com/howto/run-a-program-on-your-raspberry-pi-at-startup/#bash).
+
+# Additional optional configuration
+
+* [Install VSCode](https://code.visualstudio.com/docs/?dv=linuxarmhf_deb)
--- setup.sh
+++ setup.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
-#installing Node.js and npm
-sudo apt-get update
-sudo -s
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-sudo apt-get install -y nodejs
+if [ whoami == "root" ]; then
+echo "Error: Must not run as root"
+exit 1
+else
+echo "> Starting setup script..."
+fi
 
-#installing vim
+# setup raspberry pi
+echo "> Updating Raspberry Pi"
+sudo apt-get update && apt-get upgrade
+
+# installing
+echo "> Installing NPM/Node.js"
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
+
+echo "> Installing development tools"
 sudo apt-get update
 sudo apt-get install vim -y
+sudo apt-get install git
 
-#installing pm2
-sudo npm install -g pm2
+echo "> Installing pm2"
+npm install -g pm2
+
+# setup download directory
+echo "> Setting up download directory"
+cd ~/Desktop
+mkdir code
+cd code
 
 #daemonize app by adding this command here -  pm2 start <appName>.js
 
-#generating script that will launch pm2 on boot
-pm2 startup systemd
-pm2 save
+# generating script that will launch pm2 on boot
+# pm2 startup systemd
+# pm2 save
 
-#installing git
-sudo apt install git-all
+# # cloning dallas iot zoo app repository
+# git clone https://github.com/Internet-of-Zoo-Things/dallas-iozt-app.git
 
-#cloning dallas iot zoo app repository
-git clone https://github.com/Internet-of-Zoo-Things/dallas-iozt-app.git
-
-#installing all dependencies
-cd dallas-iozt-app
-npm install
+# # installing all dependencies
+# cd dallas-iozt-app
+# npm install
